1a24ba910072cf9f939e12a39345678b33ea7442
steam4girls <55408033+steam4girls@users.noreply.github.com>
2019-12-13 09:12:20 -0300

    Create led.ino

---
+++ led.ino
@@ -0,0 +1,32 @@
+/************************************************* *************************************************
+* OPEN-SMART Rich Shield Lesson 1:  Run four LED
+* You can learn how to turn on and turn off the LED.
+*
+* The following functions are available:
+*
+* led.on(uint8_t num);//num can be 1, 2, 3, 4 that is consistent with the LED number
+* led.off(uint8_t num);
+*
+************************************************** *******************************************************/
+
+#include <Wire.h>
+#include "RichShieldLED.h"
+
+#define LED_AMARELO  7
+#define LED_VERDE    6
+#define LED_AZUL     5
+#define LED_VERMELHO 4
+
+void setup() {
+  pinMode(LED_VERMELHO, OUTPUT);
+  pinMode(LED_VERDE, OUTPUT); 
+}
+
+void loop() {
+ 
+  digitalWrite(LED_AMARELO, HIGH);
+  delay(1000);
+  digitalWrite(LED_AMARELO, LOW);
+  delay(1000);
+ 
+}
